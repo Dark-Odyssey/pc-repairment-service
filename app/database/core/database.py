4e0591148a346582ccf13dc2ec527f02957fd2bf
Dmytro <dima.shutko5@gmail.com>
2026-04-04 09:25:43 +0200

    backend restructuring

---
+++ app/database/core/database.py
@@ -0,0 +1,30 @@
+from typing import Annotated, AsyncGenerator
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from database.core.config import settings
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL,
+    # echo=True
+)
+
+AsyncSessionGenerator = async_sessionmaker(
+    bind=async_engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    db = AsyncSessionGenerator()
+    try:
+        yield db
+    finally:
+        await db.close()
+
+
+DataBase = Annotated[AsyncSession, Depends(get_db)]
+
+class Base(DeclarativeBase):
+    pass
